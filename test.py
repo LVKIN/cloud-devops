0b129d9de7d36434574036833b6ceefc0ab8f5c9
LVKIN <backuplucas6150@gmail.com>
2021-11-22 00:33:51 -0300

    fix: indent errors fixed

--- test.py
+++ test.py
@@ -3,13 +3,12 @@ from app import app
 import unittest
 
 class Test(unittest.TestCase):
-      def setUp(self):
-              self.app = app.test_client()
+    def setUp(self):
+        self.app = app.test_client()
+        self.result = self.app.get('/')
 
-                  self.result = self.app.get('/')
+    def test_requisicao(self):
+        self.assertEqual(self.result.status_code, 200)
 
-                    def test_requisicao(self):
-                            self.assertEqual(self.result.status_code, 200)
-
-                              def test_conteudo(self):
-                                      self.assertEqual(self.result.data.decode('utf-8'), "MESSAGE")
+    def test_conteudo(self):
+        self.assertEqual(self.result.data.decode('utf-8'), "Hello World")
